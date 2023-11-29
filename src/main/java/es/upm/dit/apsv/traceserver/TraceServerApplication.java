5c9e08f58008d45d80b506d05d3386c0fd88bbc5
emilds52 <emilds@live.com>
2023-11-29 15:43:24 +0100

    init

--- src/main/java/es/upm/dit/apsv/traceserver/TraceServerApplication.java
+++ src/main/java/es/upm/dit/apsv/traceserver/TraceServerApplication.java
@@ -13,7 +13,7 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import es.upm.dit.apsv.traceserver.model.Trace;
-//import es.upm.dit.apsv.traceserver.model.TransportationOrder;
+import es.upm.dit.apsv.traceserver.model.TransportationOrder;
 import es.upm.dit.apsv.traceserver.repository.TraceRepository;
 
 @SpringBootApplication
@@ -24,6 +24,7 @@ public class TraceServerApplication {
 	
 	public static final Logger log = LoggerFactory.getLogger(TraceServerApplication.class);
 
+	@Autowired
 	private final TraceRepository tr;
 
 	public TraceServerApplication(TraceRepository tr) {
@@ -38,8 +39,27 @@ public class TraceServerApplication {
 	@Bean("consumer")
 	public Consumer<Trace> checkTrace() {
 		return t -> {
-                          log.info("Order: "+ t);
+			t.setTraceId(t.getTruck() + t.getLastSeen());
+			tr.save(t);
+			String uri = env.getProperty("orders.server");
+			RestTemplate restTemplate = new RestTemplate();
+			TransportationOrder result = null;
+
+			try {
+				result = restTemplate.getForObject(uri + t.getTruck(), TransportationOrder.class);
+			} catch (HttpClientErrorException.NotFound ex)   {
+				result = null;
+			}
+
+			if (result != null && result.getSt() == 0) {
+				result.setLastDate(t.getLastSeen());
+				result.setLastLat(t.getLat());
+				result.setLastLong(t.getLng());
+				if (result.distanceToDestination() < 10)
+					result.setSt(1);
+				restTemplate.put(uri, result, TransportationOrder.class);
+				log.info("Order updated: "+ result);
 			}
 		};
-	}
+	};
 }
